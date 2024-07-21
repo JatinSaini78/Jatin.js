670a28b3eb2dfae67bdc09df6fd7b3448b3bc484
JatinSaini78 <154868221+JatinSaini78@users.noreply.github.com>
2024-07-21 17:05:22 +0000

    day1

---
+++ Jatin/Day1.js
@@ -0,0 +1,42 @@
+//declare a number with var
+var number = 20
+
+console.log(number)
+
+let b = "Jatin"
+console.log(b)
+
+const learningJavascript = true
+
+console.log(learningJavascript)
+
+
+//let c = 50
+//let char = "saloni"
+//let boolean = true
+//let object = {Name : "Jatin", age: 25}
+//let arr = [1,2,3,4,5]
+
+//console.log(typeof c);
+//console.log(typeof char);
+//console.log(typeof boolean);
+//console.log(typeof object);
+//console.log(typeof arr);
+
+let num = 20
+    let char = "This is Day 1"
+    let bool1 = true
+    let obj = {
+        name: 'Mayank Chandel',
+        number: 10
+    }
+
+    let arr = [1, 2, 3, "Hyy", {
+        now: 'This is new'
+    }]
+
+    console.log(typeof num);
+    console.log(typeof char);
+    console.log(typeof bool1);
+    console.log(typeof obj);
+    console.log(typeof arr);
